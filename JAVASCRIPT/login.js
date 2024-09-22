6765a51e0eed7722799519b62781a15c6b95a81a
Shahadat-Hossen-008 <penaxavier078@gmail.com>
2024-09-23 02:29:52 +0600

    Add validate and navigate to home page

--- JAVASCRIPT/login.js
+++ JAVASCRIPT/login.js
@@ -1,1 +1,16 @@
+document.getElementById("btn-login").addEventListener("click", function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('input-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
 
+    // wrong way to validate
+    if(pinNumber==='1234' && phoneNumber==='019776655'){
+        window.location.href = '/home.html'
+    }
+    else{
+        alert("Wrong password or number")
+    }
+    
+    
+})
